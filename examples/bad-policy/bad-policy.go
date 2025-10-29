a701089503d3ba5b25d63efc76ad713ad2b6eb37
Brian McCallister <brianm@skife.org>
2025-10-28 20:03:38 -0700

    fixup bad-policy
    Convert to kong, make it useful for testing server setup, etc

--- examples/bad-policy/bad-policy.go
+++ examples/bad-policy/bad-policy.go
@@ -4,13 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/alecthomas/kong"
 	"github.com/epithet-ssh/epithet/pkg/ca"
 	"github.com/epithet-ssh/epithet/pkg/policy"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/lmittmann/tint"
 )
 
 const CA_PUBKEY = `ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/w2nSKNmMMEWHu9hPlNRclzsrIrxtBrlA7a+jmf/s4 test@example.com`
@@ -21,7 +25,16 @@ type RequestBody struct {
 	Connection policy.Connection `json:"connection"`
 }
 
-func main() {
+type CLI struct {
+	Verbose int `short:"v" type:"counter" help:"Increase verbosity (-v for debug, -vv for trace)"`
+
+	Principals []string `help:"principals to assign (can be specified multiple times)" short:"p" required:"true"`
+	Port       int      `help:"port to listen on" short:"P" default:"9999"`
+}
+
+var cli CLI
+
+func (c *CLI) Run(logger *slog.Logger) error {
 	r := chi.NewRouter()
 
 	// A good base middleware stack
@@ -61,7 +74,7 @@ func main() {
 		resp := &ca.PolicyResponse{
 			CertParams: ca.CertParams{
 				Identity:   "steve",
-				Names:      []string{"pe", "brianm", "stl"},
+				Names:      cli.Principals,
 				Expiration: time.Minute,
 				Extensions: map[string]string{
 					"permit-agent-forwarding": "",
@@ -85,13 +98,49 @@ func main() {
 		}
 
 		// Log the connection info for demonstration
-		fmt.Printf("Policy request for connection: %s@%s:%d (hash: %s)\n",
-			conn.RemoteUser, conn.RemoteHost, conn.Port, conn.Hash)
+		logger.Info("policy request",
+			"remote_user", conn.RemoteUser,
+			"remote_host", conn.RemoteHost,
+			"port", conn.Port,
+			"hash", conn.Hash)
 
 		w.Header().Add("Content/type", "application/json")
 		w.WriteHeader(200)
 		w.Write(out)
 	}))
 
-	http.ListenAndServe(":9999", r)
+	addr := fmt.Sprintf(":%d", cli.Port)
+	logger.Info("starting bad-policy server", "addr", addr, "principals", cli.Principals)
+	return http.ListenAndServe(addr, r)
+}
+
+func main() {
+	ktx := kong.Parse(&cli)
+	logger := setupLogger()
+	ktx.Bind(logger)
+	err := ktx.Run()
+	if err != nil {
+		logger.Error("error", "error", err)
+		os.Exit(1)
+	}
+}
+
+func setupLogger() *slog.Logger {
+	// Determine log level based on verbosity
+	level := slog.LevelWarn
+	switch cli.Verbose {
+	case 0:
+		level = slog.LevelWarn
+	case 1:
+		level = slog.LevelInfo
+	default: // 2 or more
+		level = slog.LevelDebug
+	}
+
+	logger := slog.New(tint.NewHandler(os.Stderr, &tint.Options{
+		Level:      level,
+		TimeFormat: "15:04:05",
+	}))
+
+	return logger
 }
